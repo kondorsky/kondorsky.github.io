2ba0f6a7c6f55c2247a12285f09a14a6953365ee
Konrad <konrad.kahl@targetcircle.com>
2023-08-31 15:08:14 +0200

    New build

--- assets/index-20f741bc.js
+++ assets/index-20f741bc.js
@@ -1,8 +1,8 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const l="/assets/smart-f7e1cb05.webp";document.querySelector("#app").innerHTML=`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const c of o.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();const s="/assets/smart-f7e1cb05.webp";document.querySelector("#app").innerHTML=`
 <div class="container">
         <div class="product">
             <div class="product-image">
-                <img src="${l}" alt="Product Image">
+                <img src="${s}" alt="Product Image">
             </div>
             <div class="product-details">
                 <h1 class="product-title">Smartphone XYZ</h1>
@@ -27,9 +27,9 @@
 									<input type="checkbox" id="terms" name="terms" required>
 									<label for="terms">I agree to the <a href="#">Terms and Conditions</a></label>
 								</div>
-								<div class="form-group">
-									<label for="tracking_code">Email:</label>
-									<div class="form-group__row" id="tc_tracking_code">
+								<div class="form-group" id="tc_tracking_code">
+								
+									<div class="form-group__row">
 										<button type="submit" class="tc-btn">Generate Tracking Code</button>
 									</div>
 								</div>
@@ -37,6 +37,9 @@
 						</div>
 					</div>
 			`,document.body.appendChild(r),r.querySelector(".tc-promoter-modal__overlay").addEventListener("click",function(){document.body.removeChild(r)});const e=r.querySelector("#tc_tracking_code");r.querySelector(".tc-promoter-modal__form").addEventListener("submit",function(c){c.preventDefault(),e.innerHTML=`
+			<label for="tracking_code">Your Tracking Link</label>
+			<div class="form-group__row">
 				<input value="https://c.staging.trackmytarget.com/?a=ug85il&i=faadd4" disabled type="input" id="tc_tracking_code" name="tracking_code">
 				<button id="tc_copy_tracking_code" type="button" class="tc-btn">Copy</button>
-			`;const n=e.querySelector("#tc_copy_tracking_code");n.addEventListener("click",function(){const d=e.querySelector("#tc_tracking_code");s.copyToClipboard(d.value),n.textContent="Copied",n.style.backgroundColor="#4CAF50",setTimeout(()=>{n.textContent="Copy",n.style.backgroundColor="#007bff"},2e3)})})})});const s={copyToClipboard:function(i){const t=document.createElement("textarea");t.value=i,t.style.position="fixed",t.style.opacity=0,document.body.appendChild(t),t.select(),navigator.clipboard.writeText(t.value).then(()=>{log("Text copied to clipboard")}).catch(r=>{console.error("Failed to copy text: ",r)}),document.body.removeChild(t)}};
+			</div>
+			`;const n=e.querySelector("#tc_copy_tracking_code");n.addEventListener("click",function(){const d=e.querySelector("#tc_tracking_code");l.copyToClipboard(d.value),n.textContent="Copied",n.style.backgroundColor="#4CAF50",setTimeout(()=>{n.textContent="Copy",n.style.backgroundColor="#007bff"},2e3)})})})});const l={copyToClipboard:function(i){const t=document.createElement("textarea");t.value=i,t.style.position="fixed",t.style.opacity=0,document.body.appendChild(t),t.select(),navigator.clipboard.writeText(t.value).then(()=>{log("Text copied to clipboard")}).catch(r=>{console.error("Failed to copy text: ",r)}),document.body.removeChild(t)}};
